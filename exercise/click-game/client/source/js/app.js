740e1d7d9d71a599987ae0b4a47650814cf236a4
John Häggerud <john.haggerud@lnu.se>
2015-11-30 18:44:17 +0100

    added alternative solution

--- exercise/click-game/client/source/js/app.js
+++ exercise/click-game/client/source/js/app.js
@@ -12,7 +12,18 @@ var timerElement = document.getElementById("time");
 var cg = new ClickGame(boardElement, squares, colorToClickElement, timerElement);
 cg.runGame();
 
-// I was playiong around and created two games
+
+// This code is using the module solution where the game and the timer is in
+// the modules directory
+/*
+ var cg = require("./modules/clickGameModule");
+ var boardElement = document.querySelector("#board");
+ var timerElement = document.querySelector("#time");
+ var colorToClickElement = document.querySelector("#colorToClick");
+ cg.playClickGame(boardElement, timerElement, colorToClickElement);
+ */
+
+// I was playing around and created two games
 /*
 var boardElement = document.getElementById("board2");
 var squares = document.querySelectorAll("#board2 div");
